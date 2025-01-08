be1a0c267b6af21ce3d026c7fd48983afe6317fd
bebopkenny <kennygarcia15@csu.fullerton.edu>
2025-01-07 22:21:35 -0800

    User can click on the grid to use as walls

--- pathfinding-Visualizer/src/App.tsx
+++ pathfinding-Visualizer/src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Grid } from "./components/Grid";
 import { PathfindingProvider } from "./context/PathfindingContext";
 import { SpeedProvider } from "./context/SpeedContext";
@@ -5,12 +6,14 @@ import { TileProvider } from "./context/TileContext";
 
 
 function App() {
+const isVisualizationRunningRef = useRef(false);
+
   return (
     <PathfindingProvider>
       <TileProvider>
         <SpeedProvider>
           <div className="h-screen w-screen flex flex-col">
-            <Grid />
+            <Grid isVisualizationRunningRef={isVisualizationRunningRef}/>
           </div>;
         </SpeedProvider>
       </TileProvider>
--- pathfinding-Visualizer/src/components/Grid.tsx
+++ pathfinding-Visualizer/src/components/Grid.tsx
@@ -2,10 +2,45 @@ import { twMerge } from "tailwind-merge";
 import { usePathfinding } from "../hooks/usePathfinding";
 import { MAX_COLS, MAX_ROWS } from "../utils/constants";
 import { Tile } from "./Tile";
+import { MutableRefObject, useState } from "react";
+import { checkIfStartOrEnd, createNewGrid } from "../utils/helpers";
 
 
-export function Grid() {
-    const { grid } = usePathfinding();
+export function Grid( {
+    isVisualizationRunningRef,
+}: { 
+    isVisualizationRunningRef: MutableRefObject<boolean>; 
+}) {
+    const { grid, setGrid } = usePathfinding();
+    const [isMouseDown, setIsMouseDown] = useState(false);
+
+    const handleMouseDown = (row: number, col: number) => {
+        if (isVisualizationRunningRef.current || checkIfStartOrEnd(row, col)) {
+            return;
+        }
+
+        setIsMouseDown(true);
+        const newGrid = createNewGrid(grid, row, col);
+        setGrid(newGrid);
+    };
+
+    const handleMouseUp = (row: number, col: number) => {
+        if (isVisualizationRunningRef.current || checkIfStartOrEnd(row, col)) {
+            return;
+        }
+        setIsMouseDown(false);
+        };
+
+        const handleMouseEnter = (row: number, col: number) => {
+            if (isVisualizationRunningRef.current || checkIfStartOrEnd(row, col)) {
+                return;
+            }
+
+        if (isMouseDown) {
+            const newGrid = createNewGrid(grid, row, col);
+            setGrid(newGrid);
+        }
+    };
 
     return (
         <div
@@ -20,10 +55,10 @@ export function Grid() {
                 `lg:w-[${MAX_COLS * 17}px] md:w-[${MAX_COLS * 15}px] xs:w-[${MAX_COLS * 8}px] w-[${MAX_COLS * 7}px]`
             )}
         >
-            {grid.map((row, rowIndex) => (
+            {grid.map((r, rowIndex) => (
                 <div key={rowIndex} className="flex">
-                    {row.map((tile, tileIndex) => {
-                        const { isEnd, isStart, isPath, isTraversed, isWall} = tile;
+                    {r.map((tile, tileIndex) => {
+                        const {row, col, isEnd, isStart, isPath, isTraversed, isWall} = tile;
                         return (
                             <Tile 
                                 key={tileIndex}
@@ -34,6 +69,9 @@ export function Grid() {
                                 isPath={isPath}
                                 isTraversed={isTraversed}
                                 isWall={isWall}
+                                handleMouseDown = {() => handleMouseDown(row, col)}
+                                handleMouseUp = {() => handleMouseUp(row, col)}
+                                handleMouseEnter = {() => handleMouseEnter(row, col)}
                             />
                         )
                     })}
