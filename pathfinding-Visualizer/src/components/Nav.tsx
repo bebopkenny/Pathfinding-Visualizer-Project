62ee998292725ad72f867023dd0cf837892fb798
bebopkenny <kennygarcia15@csu.fullerton.edu>
2025-01-07 23:16:38 -0800

    Created reset maze option

--- pathfinding-Visualizer/src/components/Nav.tsx
+++ pathfinding-Visualizer/src/components/Nav.tsx
@@ -1,10 +1,37 @@
+import { usePathfinding } from "../hooks/usePathfinding";
+import { MAZES } from "../utils/constants";
+import { resetGrid } from "../utils/resetGrid";
+import { Select } from "./Select";
+import { useTile } from "../hooks/useTile";
+import { MazeType } from "../utils/types";
+
 export function Nav() {
+    const { maze, setMaze, grid } = usePathfinding();
+    const { startTile, endTile} = useTile();
+
+    const handleGenerateMaze = (maze: MazeType) => {
+        if (maze === 'NONE') {
+            setMaze(maze)
+            resetGrid({grid, startTile, endTile})
+        }
+    };
+
     return (
         <div className="flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-600 sm:px-5 px-0">
             <div className="flex items-center lg:justify-between justify-center w-full sm:w[52rem]">
                 <h1 className="lg:flex hidden w-[40%] text-2xl pl-1">
                     Pathfinding Visualizer
                 </h1>
+                <div className='flex sm:items-end items-center justify-start sm:justify-between sm:flex-row flex-col sm:space-y-0 spacce-y-3 sm:py-0 py-4 sm:space-x-4'>
+                    <Select
+                        label='Maze'
+                        value={maze}
+                        options={MAZES}
+                        onChange={(e) => {
+                            handleGenerateMaze(e.target.value as MazeType);
+                        }}
+                    />
+                </div>
             </div>
         </div>
     )
